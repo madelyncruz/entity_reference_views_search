55112ce2781224576b46486e254f20eb68fb3574
Madelyn Cruz <madelyncruzmiguel@gmail.com>
2024-02-08 12:37:18 +0800

    Use entity display repository to get form mode options

--- src/Plugin/views/field/EntityFormViewsSearchSelectField.php
+++ src/Plugin/views/field/EntityFormViewsSearchSelectField.php
@@ -2,11 +2,13 @@
 
 namespace Drupal\entity_reference_views_search\Plugin\views\field;
 
+use Drupal\Core\Entity\EntityDisplayRepositoryInterface;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Link;
 use Drupal\Core\Url;
 use Drupal\views\Plugin\views\field\FieldPluginBase;
 use Drupal\views\ResultRow;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Presenting select row from results.
@@ -16,6 +18,42 @@ use Drupal\views\ResultRow;
  * @ViewsField("entity_reference_views_search_select")
  */
 class EntityFormViewsSearchSelectField extends FieldPluginBase {
+
+  /**
+   * The entity display repository service.
+   *
+   * @var \Drupal\Core\Entity\EntityDisplayRepositoryInterface
+   */
+  protected $entityDisplayRepository;
+
+  /**
+   * Constructs a new EntityFormViewsSearchSelectField object.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin ID for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\Core\Entity\EntityDisplayRepositoryInterface $entity_display_repository
+   *   The entity display repository service.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, EntityDisplayRepositoryInterface $entity_display_repository) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->entityDisplayRepository = $entity_display_repository;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('entity_display.repository'),
+    );
+  }
 
   /**
    * {@inheritdoc}
@@ -45,8 +83,9 @@ class EntityFormViewsSearchSelectField extends FieldPluginBase {
       '#required' => TRUE,
     ];
     $form['form_mode'] = [
-      '#type' => 'textfield',
+      '#type' => 'select',
       '#title' => $this->t('Form mode'),
+      '#options' => $this->getFormModes(),
       '#default_value' => $this->options['form_mode'],
       '#required' => TRUE,
     ];
@@ -100,5 +139,22 @@ class EntityFormViewsSearchSelectField extends FieldPluginBase {
    * {@inheritdoc}
    */
   public function query() {}
+
+  /**
+   * Retrieve form mode options for a specific entity type.
+   *
+   * @return array
+   *   An array of form mode options.
+   */
+  protected function getFormModes() {
+    // Get the base tables from the view.
+    $base_tables = $this->view->getBaseTables();
+
+    // Extract the entity type ID from the first table name.
+    $entity_type_id = str_replace('_field_data', '', array_key_first($base_tables));
+
+    // Retrieve form mode options using the entity type ID.
+    return $this->entityDisplayRepository->getFormModeOptions($entity_type_id);
+  }
 
 }
